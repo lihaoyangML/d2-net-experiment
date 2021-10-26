c4b0f067ddbfe5e46fa9474c252b80ec8bb5cca6
lihaoyangML <60748402+lihaoyangML@users.noreply.github.com>
2021-10-26 19:13:26 +0800

    Debugged generate_inloc_image_list.py

--- generate_inloc_image_list.py
+++ generate_inloc_image_list.py
@@ -1,5 +1,5 @@
 import os
-relative_path = "../Desktop/hierarchical_localization/Hierarchical-Localization/datasets/inloc/query/iphone7/"
+relative_path = "../hierarchical_localization/Hierarchical-Localization/datasets/inloc/query/iphone7/"
 image_paths = [relative_path + f for f in os.listdir(relative_path) if f.endswith(".JPG")]
 with open("image_list_inloc.txt", 'w') as f:
     f.write("\n".join(map(str, image_paths)))
