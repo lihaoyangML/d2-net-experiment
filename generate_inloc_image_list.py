b7673228af57befb6dc98c3d89c41a085b356597
lihaoyangML <60748402+lihaoyangML@users.noreply.github.com>
2021-10-26 19:11:57 +0800

    Add script to get inloc image list

---
+++ generate_inloc_image_list.py
@@ -0,0 +1,5 @@
+import os
+relative_path = "../Desktop/hierarchical_localization/Hierarchical-Localization/datasets/inloc/query/iphone7/"
+image_paths = [relative_path + f for f in os.listdir(relative_path) if f.endswith(".JPG")]
+with open("image_list_inloc.txt", 'w') as f:
+    f.write("\n".join(map(str, image_paths)))
